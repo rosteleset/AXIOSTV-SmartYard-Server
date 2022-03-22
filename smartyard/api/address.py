dbb95d9bde1de5cea5c2e06cfce86063d4178a69
Igor Zakharov <igor87z@gmail.com>
2022-03-23 02:44:02 +0300

    Refactor function intercom in module smartyard.api.address

--- smartyard/api/address.py
+++ smartyard/api/address.py
@@ -281,7 +281,10 @@ def get_settings_list():
 @address_branch.route("/intercom", methods=["POST"])
 def intercom():
     access_verification(request.headers)
-    if not request.get_json():
+
+    request_data = request.get_json() or {}
+    flat_id = request_data.get("flatId")
+    if not flat_id:
         abort(
             422,
             {
@@ -290,17 +293,7 @@ def intercom():
                 "message": "Необрабатываемый экземпляр",
             },
         )
-    request_data = request.get_json()
-    if not "flatId" in request_data:
-        abort(
-            422,
-            {
-                "code": 422,
-                "name": "Unprocessable Entity",
-                "message": "Необрабатываемый экземпляр",
-            },
-        )
-    flatId = request_data["flatId"]
+
     return jsonify(
         {
             "code": 200,

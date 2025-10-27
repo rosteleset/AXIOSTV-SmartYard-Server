a161c8ef1b00d3572d1ed694c55c3ca6549069c5
axiostv.ru <95581127+axiostvru@users.noreply.github.com>
2025-10-27 17:39:41 +0300

    Update smartyard_db.py
    New version

--- smartyard_db.py
+++ smartyard_db.py
@@ -1,9 +1,15 @@
 import functools, os, uuid
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, MetaData, Column, Integer, String, ForeignKey
-from sqlalchemy.dialects.postgresql import UUID, MACADDR, TIMESTAMP, ARRAY, BYTEA, INET
+from pydantic import BaseModel
+from sqlalchemy import create_engine, MetaData, Column, BigInteger, Integer, String, Text, Boolean, DateTime, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID, MACADDR, TIMESTAMP, ARRAY, BYTEA, INET, JSONB
 from sqlalchemy.sql import exists, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -13,18 +19,35 @@ else:
     print('not loaded .env file')
     exit()
 
+DATABASE_URL = "postgresql+asyncpg://" + os.getenv('PG_HCS_USER') + ":" + os.getenv('PG_HCS_PASS') + "@" + os.getenv("PG_HCS_HOST") + ":5432/" + os.getenv('PG_HCS_DBNAME')
+engine = create_async_engine(DATABASE_URL, echo=True, future=True, poolclass=NullPool)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+def async_session_generator():
+    return sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+@asynccontextmanager
+async def get_session():
+    try:
+        async_session = async_session_generator()
+        async with async_session() as session:
+            yield session
+    except:
+        await session.rollback()
+        raise
+    finally:
+        await session.close()
+
+
 database_path = "postgresql://" + os.getenv('PG_USER') + ":" + os.getenv('PG_PASS') + "@" + os.getenv("PG_HOST") + ":5432/" + os.getenv('PG_DBNAME')
 
 db = SQLAlchemy()
-
 def create_db_connection(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config['JSON_AS_ASCII'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
     db.app = app
     db.init_app(app)
-
     with app.app_context():
         db.create_all()
 
@@ -44,7 +67,7 @@ class Temps(db.Model):
 class Settings(db.Model):
     __tablename__ = 'settings'
 
-    uid= db.Column(db.Integer, primary_key=True)
+    uid= db.Column(db.BigInteger, primary_key=True)
     intercom = db.Column(db.Boolean, default=True)
     asterisk = db.Column(db.Boolean, default=True)
     w_rabbit = db.Column(db.Boolean, default=False)
@@ -76,7 +99,7 @@ class Users(db.Model):
     patronymic = db.Column(db.String(24))
     email = db.Column(db.String(60))
     videotoken = db.Column(db.String(32))
-    uid= db.Column(db.Integer)
+    uid= db.Column(db.BigInteger)
     vttime = db.Column(db.DateTime(timezone=False))
     strims = db.Column(db.ARRAY(String))
     pushtoken = db.Column(db.Text)
@@ -115,7 +138,7 @@ class Records(db.Model):
     __tablename__ = 'records'
 
     id = db.Column(db.Integer, nullable=False, autoincrement=True, primary_key=True)
-    uid = db.Column(db.Integer)
+    uid = db.Column(db.BigInteger)
     url = db.Column(db.Text, index=True, unique=True )
     fileurl = db.Column(db.Text, index=True, unique=True)
     rtime = db.Column(db.DateTime(timezone=False), server_default=func.current_timestamp()) 
@@ -139,12 +162,14 @@ class Invoices(db.Model):
     invoice_time = db.Column(db.DateTime(timezone=False), server_default=func.current_timestamp())
     invoice_pay = db.Column(db.Boolean, default=False)
     contract = db.Column(db.Text, index=True)
-    
-    def __init__(self, invoice_id, invoice_time, invoice_pay, contract):
+    amount = db.Column(db.Integer, nullable=True)
+
+    def __init__(self, invoice_id, invoice_time, invoice_pay, contract, amount):
         self.invoice_id = invoice_id
         self.invoice_time = invoice_time
         self.invoice_pay = invoice_pay
         self.contract = contract
+        self.amount = amount
 
     def __repr__(self):
         return f""
@@ -165,30 +190,30 @@ class Types(db.Model):
 class Devices(db.Model):
     __tablename__ = 'devices'
 
-    device_id = db.Column(db.Integer, nullable=False, autoincrement=True, primary_key=True)
-    device_uuid = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    device_mac = db.Column(MACADDR(), nullable=True)
-    device_type = db.Column(db.Integer, ForeignKey('types.id', ondelete='CASCADE'), nullable=False)
-    affiliation = db.Column(db.Integer, nullable=True)
-    owner = db.Column(db.Integer, nullable=True)
-    url = db.Column(db.String(64), nullable=True)
-    port = db.Column(db.Integer, nullable=True)
-    stream = db.Column(db.String(64), nullable=True)
-    is_active = db.Column(db.Boolean, default=False, nullable=False)
-    title = db.Column(db.String(64), nullable=True)
-    address = db.Column(db.String(110), nullable=True)
-    longitude = db.Column(db.Numeric(12,9), nullable=True)
-    latitude = db.Column(db.Numeric(12,9), nullable=True)
-    server_id = db.Column(db.Integer, nullable=True)
-    tariff_id = db.Column(db.Integer, nullable=True)
-    domophoneid = db.Column(db.Integer, nullable=True)
-    sippassword = db.Column(db.Text, nullable=True)
-    dtmf = db.Column(db.Integer, nullable=True)
-    camshot = db.Column(db.Text, nullable=True)
-    paneltype = db.Column(db.Integer, nullable=True)
-    panelip = db.Column(INET(), nullable=True)
-    panellogin = db.Column(db.Text, nullable=True)
-    panelpasswd = db.Column(db.Text, nullable=True)
+    device_id = db.Column(db.Integer, nullable=False, autoincrement=True, primary_key=True) # ID девайса
+    device_uuid = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4) # UUID девайса
+    device_mac = db.Column(MACADDR(), nullable=True) # MAC адрес девайса, заполняется только для камер подключенных по SRT
+    device_type = db.Column(db.Integer, ForeignKey('types.id', ondelete='CASCADE'), nullable=False) # 1-камера 2-замок 3-srt камера
+    affiliation = db.Column(db.Integer, nullable=True) # Fatkbhjdfyyjcnm, необязаьтельное поле
+    owner = db.Column(db.Integer, nullable=True) # Пользователь, необязательное поле
+    url = db.Column(db.String(64), nullable=True) # УРЛ на видеосервере
+    port = db.Column(db.Integer, nullable=True) # Порт на видеосервере, только для srt камер
+    stream = db.Column(db.String(64), nullable=True) # Адрес rtsp потока камеры
+    is_active = db.Column(db.Boolean, default=False, nullable=False) # Включена ли камера
+    title = db.Column(db.String(64), nullable=True) # Описание, видно в МП
+    address = db.Column(db.String(110), nullable=True) # Адрес девайса, обязательное поле, по нему идет поиск устройств
+    longitude = db.Column(db.Numeric(12,9), nullable=True) # Географические координаты для отображения в МП, обязательное поле
+    latitude = db.Column(db.Numeric(12,9), nullable=True) # Географические координаты для отображения в МП, обязательное поле
+    server_id = db.Column(db.Integer, nullable=True) # ID видеосервера, не обязательное поле
+    tariff_id = db.Column(db.Integer, nullable=True) # ID тарифа, не обязательное поле
+    domophoneid = db.Column(db.Integer, nullable=True) # Логин SIP. Внимание, на панеле эта цифра + 100000
+    sippassword = db.Column(db.Text, nullable=True) # Gfhjkm ЫШЗ
+    dtmf = db.Column(db.Integer, nullable=True) # Код открытия
+    camshot = db.Column(db.Text, nullable=True) # УРЛ получения стоп кадра с камеры
+    paneltype = db.Column(db.Integer, nullable=True) # 1-Бевард 2-Сокол
+    panelip = db.Column(INET(), nullable=True) # IP адрес домофона
+    panellogin = db.Column(db.Text, nullable=True) # Логин консоли домофона
+    panelpasswd = db.Column(db.Text, nullable=True) # Пароль консоли домофона
 
     def __init__(self, device_id, device_uuid, device_mac, device_type, affiliation, owner, url, port, stream, is_active, title, address, longitude, latitude, server_id, tariff_id, domophoneid, sippassword, dtmf, camshot, paneltype, panelip, panellogin, panelpasswd):
         self.device_id = device_id
@@ -222,12 +247,12 @@ class Devices(db.Model):
 class Rights(db.Model):
     __tablename__ = 'rights'
 
-    uid = db.Column(db.Integer, nullable=False, primary_key=True)
+    uid = db.Column(db.BigInteger, nullable=False, primary_key=True)
     uid_right = db.Column(db.ARRAY(Integer), nullable=True)
 
     def __init__(self, uid, uid_right):
-        self.uid = uid
-        self.uid_right = uid_right
+        self.uid = uid # UID пользователя
+        self.uid_right = uid_right # массив ID девайсев, назначенных пользователю
 
     def __repr__(self):
         return f""
@@ -236,13 +261,15 @@ class Doors(db.Model):
     __tablename__ = 'doors'
 
     id = db.Column(db.Integer, nullable=False, autoincrement=True, primary_key=True)
-    open = db.Column(db.String(64), nullable=False)
-    device_id = db.Column(db.Integer, ForeignKey('devices.id', ondelete='CASCADE'), nullable=False)
-    cam = db.Column(db.Integer, ForeignKey('devices.id', ondelete='CASCADE'), nullable=False)
-    icon = db.Column(db.String(14), nullable=False)
-    entrance = db.Column(db.Integer, nullable=False)
-    name = db.Column(db.Text, nullable=False)
-    open_trait = db.Column(db.Text, nullable=True)
+    open = db.Column(db.String(64), nullable=False) # УРЛ открытия
+    device_id = db.Column(db.Integer, ForeignKey('devices.device_id', ondelete='CASCADE'), nullable=False) # ID в талице devuces как замка
+    cam = db.Column(db.Integer, ForeignKey('devices.device_id', ondelete='CASCADE'), nullable=False) # ID в талице devuces как камеры
+#    device_id = db.Column(db.Integer, nullable=False) # ID в талице devuces как замка
+#    cam = db.Column(db.Integer, nullable=False) # ID в талице devuces как камеры
+    icon = db.Column(db.String(14), nullable=False) # Иконка в МП
+    entrance = db.Column(db.Integer, nullable=False) # Подъезд, не обязательное поле
+    name = db.Column(db.Text, nullable=False) # Название
+    open_trait = db.Column(db.Text, nullable=True) # тип запроса на открытие (GET, PUT, POST)
     def __init__(self, id, open, device_id, cam, icon, entrance, name, open_trait):
         self.id = id
         self.open = open
@@ -294,3 +321,37 @@ class Upgrade(db.Model):
     def __repr__(self):
         return f""
 
+Base = declarative_base()
+
+class Appeals(Base):
+    __tablename__ = "appeals"
+    appealUuid = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    createdUtc = Column(DateTime(timezone=False), server_default=func.timezone('UTC', func.current_timestamp(0)))
+    phone = Column(BigInteger, index=True)
+    uid = Column(Integer, index=True)
+    addressUuid = Column(BigInteger, index=True)
+    address = Column(Text, index=True)
+    createdDoc = Column(Text)
+    createdSound = Column(Text)
+    appealId = Column(BigInteger, index=True)
+    companyId = Column(Integer, index=True)
+    parentAppeal = Column(UUID(as_uuid=True), index=True)
+    category = Column(Integer, index=True)
+    description = Column(Text)
+    patternDisc = Column(Text)
+    status = Column(Integer, index=True)
+    executorId = Column(Integer, index=True)
+    statusDescription = Column(ARRAY(JSONB))
+    workerDescription = Column(ARRAY(JSONB))
+
+class Categorys(Base):
+    __tablename__ = "categorys"
+    categoryId = Column(Integer, primary_key=True, index=True)
+    categoryName = Column(String, index=True)
+    companyId = Column(Integer, index=True)
+
+class Statuss(Base):
+    __tablename__ = "statuss"
+    statusId = Column(Integer, primary_key=True, index=True)
+    statusName = Column(String, index=True)
+    companyId = Column(Integer, index=True)
